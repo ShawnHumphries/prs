8f732314c91c9181ca83156220f2a6c0db6b5344
ShawnHumphries <sahump1@gmail.com>
2017-06-03 21:52:44 -0400

    Adding JavaScript files to initial commit

---
+++ WebContent/main.js
@@ -0,0 +1,19 @@
+/**
+ * Main JavaScript source for the prs application.
+ */
+
+function goBack() {
+	window.history.back();
+}
+
+$(document).ready( function() {
+    var now = new Date();
+ 
+    var day = ("0" + now.getDate()).slice(-2);
+    var month = ("0" + (now.getMonth() + 1)).slice(-2);
+
+    var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+
+
+   $('#datePicker').val(today);
+});
